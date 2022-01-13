3792c6ad90077ddb30f1baf2220646d7c8f38cc4
Bisquitman <lynx8309@gmail.com>
2022-01-13 16:21:24 +0300

    Final

--- js/radioPlayer.js
+++ js/radioPlayer.js
@@ -1,1 +1,82 @@
-export const radioPlayerInit = () => { };
+export const radioPlayerInit = () => {
+  const radio = document.querySelector('.radio');
+  const radioCoverImg = document.querySelector('.radio-cover__img');
+  const radioNavigation = document.querySelector('.radio-navigation');
+  const radioHeaderBig = document.querySelector('.radio-header__big');
+  const radioItem = document.querySelectorAll('.radio-item');
+  const radioStop = document.querySelector('.radio-stop');
+  const radioVolume = document.querySelector('.radio-volume');
+  const radioMute = document.querySelector('.radio-mute');
+
+  let prevVolume = 1;
+
+  const audio = new Audio();
+  audio.type = 'audio/aac';
+
+  radioStop.disabled = true;
+
+  const changePlayIcon = () => {
+    if (audio.paused) {
+      radio.classList.remove('play');
+      radioStop.classList.add('fa-play');
+      radioStop.classList.remove('fa-stop');
+    } else {
+      radio.classList.add('play');
+      radioStop.classList.add('fa-stop');
+      radioStop.classList.remove('fa-play');
+    }
+  };
+
+  const selectItem = (elem) => {
+    radioItem.forEach(item => item.classList.remove('select'));
+    elem.classList.add('select');
+  };
+
+  radioNavigation.addEventListener('change', (event) => {
+    const target = event.target;
+    const parent = target.closest('.radio-item');
+    const title = parent.querySelector('.radio-name').textContent;
+    const urlImg = parent.querySelector('.radio-img').src;
+
+    selectItem(parent);
+    radioHeaderBig.textContent = title;
+    radioCoverImg.src = urlImg;
+
+    radioStop.disabled = false;
+    audio.src = target.dataset.radioStation;
+    audio.play();
+    changePlayIcon();
+  });
+
+  radioStop.addEventListener('click', () => {
+    if (audio.paused) {
+      audio.play();
+    } else {
+      audio.pause();
+    }
+    changePlayIcon();
+  });
+
+  radioVolume.addEventListener('input', () => {
+    audio.volume = radioVolume.value / 100;
+    prevVolume = audio.volume;
+  });
+
+  audio.volume = 0.5;
+
+  radioVolume.value = audio.volume * 100;
+
+  radioMute.addEventListener('click', () => {
+    if (audio.volume) {
+      prevVolume = audio.volume;
+      audio.volume = 0;
+    } else {
+      audio.volume = prevVolume;
+    }
+  });
+
+  radioPlayerInit.stop = () => {
+    audio.pause;
+    changePlayIcon();
+  };
+};
--- js/videoPlayer.js
+++ js/videoPlayer.js
@@ -1,1 +1,84 @@
-export const videoPlayerInit = () => { };
+import { addZero } from "./functions.js";
+
+export const videoPlayerInit = () => {
+  const videoPlayer = document.querySelector('.video-player');
+  const videoButtonPlay = document.querySelector('.video-button__play');
+  const videoButtonStop = document.querySelector('.video-button__stop');
+  const videoTimePassed = document.querySelector('.video-time__passed');
+  const videoProgress = document.querySelector('.video-progress');
+  const videoTimeTotal = document.querySelector('.video-time__total');
+
+  const videoFullscreen = document.querySelector('.video-fullscreen');
+  const videoVolume = document.querySelector('.video-volume');
+
+  const toggleIcon = () => {
+    if (videoPlayer.paused) {
+      videoButtonPlay.classList.remove('fa-pause');
+      videoButtonPlay.classList.add('fa-play');
+    } else {
+      videoButtonPlay.classList.add('fa-pause');
+      videoButtonPlay.classList.remove('fa-play');
+    }
+  };
+
+  const togglePlay = () => {
+    if (videoPlayer.paused) {
+      videoPlayer.play();
+    } else {
+      videoPlayer.pause();
+    };
+  }
+
+  const stopPlay = () => {
+    videoPlayer.pause();
+    videoPlayer.currentTime = 0;
+  };
+
+  videoPlayer.addEventListener('click', togglePlay);
+  videoButtonPlay.addEventListener('click', togglePlay);
+
+  videoPlayer.addEventListener('play', toggleIcon);
+  videoPlayer.addEventListener('pause', toggleIcon);
+
+  videoButtonStop.addEventListener('click', stopPlay);
+
+  videoPlayer.addEventListener('timeupdate', () => {
+    const currentTime = videoPlayer.currentTime;
+    const duration = videoPlayer.duration;
+
+    videoProgress.value = (currentTime / duration) * 100;
+
+    let minutesPassed = Math.floor(currentTime / 60);
+    let secondsPassed = Math.floor(currentTime % 60);
+    let minutesTotal = Math.floor(duration / 60);
+    let secondsTotal = Math.floor(duration % 60);
+
+    videoTimePassed.textContent = `${addZero(minutesPassed)}:${addZero(secondsPassed)}`;
+    videoTimeTotal.textContent = `${addZero(minutesTotal)}:${addZero(secondsTotal)}`;
+  });
+
+  videoProgress.addEventListener('input', () => {
+    const duration = videoPlayer.duration;
+    const value = videoProgress.value;
+
+    videoPlayer.currentTime = (value * duration) / 100;
+  });
+
+  videoFullscreen.addEventListener('click', () => {
+    videoPlayer.requestFullscreen();
+  });
+
+  videoVolume.addEventListener('input', () => {
+    videoPlayer.volume = videoVolume.value / 100;
+  });
+
+  videoPlayer.volume = 0.5;
+
+  videoVolume.value = videoPlayer.volume * 100;
+
+  videoPlayerInit.stop = () => {
+    if (!videoPlayer.paused) {
+      stopPlay();
+    }
+  };
+};
